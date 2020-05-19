e215ac2d0794f93ce231cc79aa7d50c946309474
Roy <Roy.Rosenberg@live.com>
2020-05-19 17:48:36 +0300

    added basic structure modules, componenets

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+
+import { DefaultComponent } from './default.component';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class DefaultModule { }
