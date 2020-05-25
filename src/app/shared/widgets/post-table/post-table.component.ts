27c5b2c19d181d31044ffff24cc2c94ac18d572c
Roy <Roy.Rosenberg@live.com>
2020-05-25 07:28:12 +0300

    added collapse expand option

--- src/app/shared/widgets/post-table/post-table.component.ts
+++ src/app/shared/widgets/post-table/post-table.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -8,7 +9,14 @@ import { PagingInfo } from '../../models/postFilter';
 @Component({
   selector: 'app-post-table',
   templateUrl: './post-table.component.html',
-  styleUrls: ['./post-table.component.css']
+  styleUrls: ['./post-table.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class PostTableComponent implements OnInit {
   @Input('posts')
@@ -28,7 +36,8 @@ export class PostTableComponent implements OnInit {
   @Input() loadingInProgress: boolean;
   @Output() paginatorChanged = new EventEmitter<PagingInfo>();
   dataSource = new MatTableDataSource<Post>([]);
-  displayedColumns: string[] = ['id', 'title', 'date', 'userId'];
+  expandedElement: Post;
+  displayedColumns: string[] = ['id', 'title', 'date', 'user', 'customer'];
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor() { }
